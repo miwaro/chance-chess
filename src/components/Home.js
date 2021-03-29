11f0558c6dc9d55679e029e0c9b4281d04f93eba
shanehobson1 <shane@democracylive.com>
2021-03-28 23:53:04 -0700

    Add more redundancy

--- src/components/Home.js
+++ src/components/Home.js
@@ -115,8 +115,22 @@ const Home = (props) => {
       socket.emit('new move', { ...newState });
 
       setTimeout(() => {
+        console.log(socket)
+        console.log('2nd move update', newState)
         socket.emit('new move', { ...newState });
       }, 1000)
+
+      setTimeout(() => {
+        console.log(socket)
+        console.log('3nd move update', newState)
+        socket.emit('new move', { ...newState });
+      }, 1500)
+
+      setTimeout(() => {
+        console.log(socket)
+        console.log('3rd move update', newState)
+        socket.emit('new move', { ...newState });
+      }, 2000)
     }
   }, [props.whiteToMove])
 
